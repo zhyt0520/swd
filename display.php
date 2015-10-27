5c5cbcff08f3e6772d8e7a08f10201a123e5fecf
zhyt <zhyt0520@gmail.com>
2015-10-27 23:02:57 +0800

    first sync

---
+++ display.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>display</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<?php require "config.php" ?>
+	<?php require "functions.php" ?>
+</head>
+<body>
+	<div id="top"></div>
+	<div id="left"></div>
+	<div id="right"></div>
+	<script type="text/javascript" src="swa.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>single well data</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<?php require "config.php" ?>
+	<?php require "functions.php" ?>
+</head>
+<body>
+	<div id="query"></div>
+	<script type="text/javascript" src="swa.js"></script>
+</body>
+</html>
