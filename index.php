8c9f18dea3e60f6469a1c21bd3eecb7d79dd00f0
zhyt <zhyt0520@gmail.com>
2015-10-29 00:29:27 +0800

    don't use chinese in mysql...

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 <html>
 <head>
 	<title>single well data</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel='stylesheet' type='text/css' href='style.css'>
 	<style>body{text-align: center;}</style>
-	<?php require "config.php" ?>
-	<?php require "functions.php" ?>
+	<?php require 'config.php' ?>
+	<?php require 'functions.php' ?>
+	<?php $conn=connect_db(); ?>
 </head>
 <body>
-	<div id="query"></div>
-	<script type="text/javascript" src="swa.js"></script>
+	<div id='query'></div>
+	<script type='text/javascript' src='swa.js'></script>
 </body>
 </html>
